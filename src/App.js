d1070619b9cae52976ca1ee2a8196d3bf7a6995c
Rezan <rezanicgil@gmail.com>
2025-06-29 18:48:59 +0300

    note: useEffect cleanup  function

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@ import { useState, useEffect } from 'react';
 function App() {
 
   const [resourceType, setResourceType] = useState('posts');
-  const [items, setItems] = useState([]);
-
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(data => setItems(data));
+    console.log('useEffect called');
+
+    return () => {
+      console.log('Cleanup function called');
+    }
   }, [resourceType]);
+
 
   return (
     <>
       <div>
-        <button onClick= {() => setResourceType('posts')}>Posts</button>
-        <button onClick= {() => setResourceType('users')}>Users</button>
-        <button onClick= {() => setResourceType('comments')}>Comments</button>
-      </div> 
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
       <h1>{resourceType}</h1>
-      {
-        items.map(item => (
-          <pre key={item.id}>{JSON.stringify(item, null, 2)}</pre>
-        ))
-      }
-   </>
+    </>
   );
 }
 
