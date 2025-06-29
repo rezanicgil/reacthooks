ba644c46f94797a20b346082f14cdd29ec5ce030
Rezan <rezanicgil@gmail.com>
2025-06-29 18:26:01 +0300

    note: useState lesson done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue');
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1);
+    setTheme('red');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+   </>
   );
 }
 
